45d3050450c90e44ae0b2cc579d91878529a7190
smapurbo23 <smapurbo23@gmail.com>
2025-01-30 17:52:24 +0600

    contactBook.py

--- contactBook.py
+++ contactBook.py
@@ -1,15 +1,26 @@
 # User Choosing Options
-n =
-while n:
-    print("What do you want to do? \n 1.Add new contact\n 2.Update contact\n 3.Delete contact\n 4.Search contact\n")
-    user = int(input("Enter your option: "))
+print("What do you want to do? \n 1.Add new contact\n 2.Update contact\n 3.Delete contact\n 4.Search contact\n")
+user = int(input("Enter your option: "))
 
-    names = []
-    phoneNumbers = []
-    emails = []
+names = []
+phoneNumbers = []
+emails = []
 
-    if user == 1:
+if user == 1:
 
+    name = input("Name: ")
+    phone_number = input("Phone Number: ")  # for convert to int => int(input("Phone Number: "))
+    email = input("Email: ")
+
+    names.append(name)
+    phoneNumbers.append(phone_number)
+    emails.append(email)
+    print("Contact added successfully")
+
+if user == 2:
+    update = int(input("Which contact do you want to update? \n"))
+
+    if update in names:
         name = input("Name: ")
         phone_number = input("Phone Number: ")  # for convert to int => int(input("Phone Number: "))
         email = input("Email: ")
@@ -17,22 +28,9 @@ while n:
         names.append(name)
         phoneNumbers.append(phone_number)
         emails.append(email)
-        print("Contact added successfully")
 
-    if user == 2:
-        update = int(input("Which contact do you want to update? \n"))
-
-        if update in names:
-            name = input("Name: ")
-            phone_number = input("Phone Number: ")  # for convert to int => int(input("Phone Number: "))
-            email = input("Email: ")
-
-            names.append(name)
-            phoneNumbers.append(phone_number)
-            emails.append(email)
-
-            print("Contact updated successfully")
-        else:
+        print("Contact updated successfully")
+    else:
         print("Invalid Input!")
 
 # if user = 3:
@@ -47,6 +45,3 @@ while n:
 #             print(names.)
 a = 1
 b = 1
-
-print(a==b)
-print("My name is Shawal".strip("S"))
